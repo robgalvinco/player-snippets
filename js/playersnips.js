e337a900607e8261bdab6564660cd5f5c26abcc0
Robert Galvin <robertgalvin@Roberts-Air.attlocal.net>
2022-10-31 18:13:50 -0400

    updated playersnips.js links

--- js/playersnips.js
+++ js/playersnips.js
@@ -6,12 +6,12 @@ $( document ).ready(function() {
         var f = document.getElementsByTagName("script")[0];
             var playerbs_css = document.createElement("link");
             playerbs_css.rel = "stylesheet";
-            playerbs_css.href = "https://assets-robgalvinco.netlify.app/playersnips/kapow-playerbs.css";
+            playerbs_css.href = "https://cdn.jsdelivr.net/gh/jquery/jquery/player-snippets/css/kapow-playerbs.css";
             f.parentNode.insertBefore(playerbs_css, f);
 
            var snipps_css = document.createElement("link");
            snipps_css.rel = "stylesheet";
-           snipps_css.href = "https://assets-robgalvinco.netlify.app/playersnips/text.css";
+           snipps_css.href = "https://cdn.jsdelivr.net/gh/jquery/jquery/player-snippets/css/text.css";
            f.parentNode.insertBefore(snipps_css, f);
    }     
     
